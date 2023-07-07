7653cf5ce2df5297c53e94958420bb7814a3870a
Jing Yu Koh <kohjingyu95@gmail.com>
2023-07-07 11:19:57 -0400

    Update running instructions.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 """Training example.
 
 Example usage:
+  python -u main.py \
+      --dataset=cc3m  --val-dataset=cc3m \
+      --opt-version='facebook/opt-6.7b' --visual-model='openai/clip-vit-large-patch14' \
+      --exp_name='gill_exp'   --log-base-dir='runs/' \
+      --batch-size=64  --val-batch-size=64  --precision='bf16'
 
-python -u main.py \
-    --dataset=cc3m  --val-dataset=cc3m \
-    --opt-version='facebook/opt-6.7b' --visual-model='openai/clip-vit-large-patch14' \
-    --exp_name='fromage_exp'   --log-base-dir='runs/' \
-    --batch-size=64  --val-batch-size=64  --precision='bf16'
-
-Example run on 2 A6000 GPUs to reproduce the model in the paper:
-
+Example run on 2 A6000 GPUs to reproduce the paper results:
+  randport=$(shuf -i8000-9999 -n1)  # Generate a random port number
+  python -u main.py \
+      --dist-url "tcp://127.0.0.1:${randport}" --dist-backend 'nccl' \
+      --multiprocessing-distributed --world-size 1 --rank 0 \
+      --dataset=cc3m  --val-dataset=cc3m \
+      --exp-name='gill_exp' --image-dir='data/'  --log-base-dir='runs/' \
+      --precision='bf16'  --print-freq=100
 """
 import argparse
 from collections import OrderedDict
