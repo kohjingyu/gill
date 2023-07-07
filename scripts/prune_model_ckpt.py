7e9a9bcd88e0db1449f9bac347b493ee9ac4220d
Jing Yu Koh <kohjingyu95@gmail.com>
2023-07-07 17:41:12 -0400

    Update prune_model_ckpt script.

--- scripts/prune_model_ckpt.py
+++ scripts/prune_model_ckpt.py
@@ -1,7 +1,7 @@
 """Prunes model weights to keep just the necessary trained weights.
 
 Example usage:
-  python gill/prune_model_ckpt.py  runs/gill_exp
+  python scripts/prune_model_ckpt.py  runs/gill_exp
 """
 
 import json
@@ -19,19 +19,18 @@ if __name__ == '__main__':
     with open(os.path.join(model_dir, 'model_args.json'), 'rb') as f:
         model_args = json.load(f)
 
-    assert model_args['share_ret_gen']
-    assert model_args['num_gen_tokens'] == model_args['num_ret_tokens']
-
     del checkpoint['epoch']
     del checkpoint['best_acc1']
     del checkpoint['optimizer']
     del checkpoint['scheduler']
 
+    state_dict = {}
+    for k, v in checkpoint['state_dict'].items():
+        state_dict[k.replace('module.', '')] = v.detach().clone()
+
+    checkpoint['state_dict'] = state_dict
+    finetuned_tokens = checkpoint['state_dict']['model.input_embeddings.weight'][-model_args['num_gen_tokens']:, :].detach().clone()
+    checkpoint['state_dict']['model.input_embeddings.weight'] = finetuned_tokens
+
     with open(os.path.join(model_dir, 'pretrained_ckpt.pth.tar'), 'wb') as f:
-        for k, v in checkpoint['state_dict'].items():
-            checkpoint['state_dict'][k.replace('module.', '')] = v.detach().clone()
-
-        finetuned_tokens = checkpoint['state_dict']['model.input_embeddings.weight'][-model_args['num_gen_tokens']:, :].detach().clone()
-        checkpoint['state_dict']['model.input_embeddings.weight'] = finetuned_tokens
-
         torch.save(checkpoint, f)
